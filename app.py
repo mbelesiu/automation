7c89a13c32c0b89f7797d3ff6cc30395e82e81d1
mbelesiu <matt.belesiu@gmail.com>
2022-11-26 15:11:55 -0800

    magic 8 ball

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-#test again again again
+import random
+
+messages = ['It is certain',
+            'It is decidedly so',
+            'Yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concentrate and ask again',
+            'My reply is no',
+            'Outlook not so good',
+            'Very doubtful']
+
+print(messages[random.randint(0, len(messages)-1)])
