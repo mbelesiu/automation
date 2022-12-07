5efb6c29bc8fc70d199a089048149f8db5d43571
mbelesiu <matt.belesiu@gmail.com>
2022-12-07 15:23:25 -0800

    init

---
+++ bulletPointAdder/bulletPointAdder.py
@@ -0,0 +1,12 @@
+#! python3
+
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+
+text = pyperclip.paste()
+
+# TODO: Seperate lines and add stars.
+
+pyperclip.copy(text)
