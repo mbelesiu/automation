bfec9f06fb071d04ac01a587d3b514f7001b3214
mbelesiu <matt.belesiu@gmail.com>
2022-12-08 15:50:39 -0800

    finished my list

--- bulletPointAdder/bulletPointAdder.py
+++ bulletPointAdder/bulletPointAdder.py
@@ -7,6 +7,12 @@ import pyperclip
 
 text = pyperclip.paste()
 
-# TODO: Seperate lines and add stars.
+# Seperate lines and add stars.
 
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+    
+text = '\n'.join(lines)
 pyperclip.copy(text)
+
