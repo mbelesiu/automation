095e478f39609ba03c849d2ad5c09a8b7d1861fb
mbelesiu <matt.belesiu@gmail.com>
2023-04-24 14:57:17 -0400

    more regex

--- regex/isPhoneNumber.py
+++ regex/isPhoneNumber.py
@@ -1,1 +1,14 @@
 import re
+
+phoneNumRegex = re.compile(r'(\d\d\d-)?(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 503-550-7997')
+mo2 = phoneNumRegex.search('My number is 550-7997')
+print('Phone number found: ' + mo.group())
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
+areaCode, mainNumber = mo.groups()
+print(areaCode)
+print(mainNumber)
+print("Mo2: " + mo2.group())
